4e303b9f0cb9cd4b3a6397b0905e49adae2a7dcc
Jiyun Xu <xjy2017cn@hotmail.com>
2020-01-31 16:36:18 -0800

    Updates

---
+++ precache-manifest.4b944afcfa5e34f3e92e83318ab9ce56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28355bab0f46d19874bdc4fd59164dba",
+    "url": "/SSI-react-project/index.html"
+  },
+  {
+    "revision": "d80bafb37be65b998c05",
+    "url": "/SSI-react-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "820da14ffce4ac3587cc",
+    "url": "/SSI-react-project/static/js/2.1ed48805.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SSI-react-project/static/js/2.1ed48805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80bafb37be65b998c05",
+    "url": "/SSI-react-project/static/js/main.82690bda.chunk.js"
+  },
+  {
+    "revision": "41642b4b45da162e19fe",
+    "url": "/SSI-react-project/static/js/runtime-main.87ef9aee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SSI-react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
